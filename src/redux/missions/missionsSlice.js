0a705a81a89ebec69966c8e7b998ba5df12bcf5f
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-29 21:03:36 +0530

    add async function to fetch missions data

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchMissionsData = createAsyncThunk(
+  'missions/fetchMission',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios('https://api.spacexdata.com/v3/missions');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  },
+);
 
 const initialState = {
   missions: [],
@@ -10,7 +23,29 @@ const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchMissionsData.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchMissionsData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const fetchedData = [];
+        action.payload.forEach((mission) => {
+          const data = {
+            mission_id: mission.mission_id,
+            mission_name: mission.mission_name,
+            mission_description: mission.description,
+          };
+          fetchedData.push(data);
+        });
+        state.missions = fetchedData;
+      })
+      .addCase(fetchMissionsData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload.message;
+      });
+  },
 });
 
 export default missionsSlice.reducer;
