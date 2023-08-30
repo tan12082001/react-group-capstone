c2e99a7a2cc2291d37d0f8f9c2ce531616b2e845
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-30 16:18:31 +0530

    Add join mission reducer that changes missionJoin key value

--- src/Components/MissionComponents/EachMission.js
+++ src/Components/MissionComponents/EachMission.js
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../../redux/missions/missionsSlice';
 
 const EachMission = ({ mission }) => {
   const {
     missionName,
     missionDescription,
+    missionId,
   } = mission;
+
+  const dispatch = useDispatch();
 
   return (
     <tr className="each-mission">
       <td>{missionName}</td>
       <td>{missionDescription}</td>
       <td><span className="not-a-member">NOT A MEMBER</span></td>
-      <td><button className="join-mission" type="button">JOIN MISSION</button></td>
+      <td><button className="join-mission" type="button" onClick={() => dispatch(joinMission(missionId))}>JOIN MISSION</button></td>
     </tr>
   );
 };
@@ -20,6 +25,7 @@ EachMission.propTypes = {
   mission: PropTypes.shape({
     missionName: PropTypes.string.isRequired,
     missionDescription: PropTypes.string.isRequired,
+    missionId: PropTypes.string.isRequired,
   }).isRequired,
 };
 
